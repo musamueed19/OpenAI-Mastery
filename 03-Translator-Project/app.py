2254ce42a0f1cb80c41de71fd96b32ed90bd1379
Muhammad Musa Mueed - Mac <musamueed19>
2026-02-07 17:05:10 +0500

    flask project setuped, part1

--- 03-Translator-Project/app.py
+++ 03-Translator-Project/app.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/", methods=["GET"])
+def index():
+    # render_template() function from flask library is
+    # - to render the template (path as specified in the template_folder parameter of the Flask app) and return it as a response to the client.
+    return render_template("index.html")
+
+# python app.py (Start the Flask application)
+if __name__ == "__main__":
+    app.run(debug=True)
